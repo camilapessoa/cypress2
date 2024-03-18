18a226ea860ba98b1081090b13c8c7a9599cf41b
camilapessoa <camilabentespessoa@gmail.com>
2024-03-18 10:13:51 -0300

    cadastro massa mochawesome .env

--- cypress/e2e/cadastro-massa.cy.js
+++ cypress/e2e/cadastro-massa.cy.js
@@ -1,5 +1,6 @@
 describe('Página de cadastro', () => {
     beforeEach(() =>{
+      cy.fixture('clinica').as('clinicaJson')
       cy.visit('/');
       })
     
@@ -23,30 +24,36 @@ describe('Página de cadastro', () => {
       
             cy.get('.sc-laZRCg').should('exist').should('be.visible') //garante que existe e não está abaixo de outra classe e que está visível ao usuário
             cy.get(':nth-child(3) > .MuiStepLabel-root > .MuiStepLabel-iconContainer > .sc-hiDMwi').should('be.visible') //teste de componente, não será abordado no curso -> para saber mais
+    
           })
       
       
           it('cadastra usuário', () =>{
-            cy.get('[href="/cadastro"]').click();
-            cy.get('[data-test="inputNome"]').type('Catarina P');
-            cy.get('[data-test="inputCNPJ"]').type('12598432');
-            cy.get('[data-test="inputEmail"]').type('catarina@email.com');
-            cy.get('[data-test="inputSenha"]').type('Senha123');
-            cy.get('[data-test="inputSenhaVerificada"]').type('Senha123');
-            cy.get('.sc-bcXHqe').click()
-            cy.get('[data-test="inputTelefone"]').type('9999999999');
-            cy.get('[data-test="inputCEP"]').type('99999999');
-            cy.get('[data-test="inputRua"]').type('Salvatori')
-            cy.get('[data-test="inputNumero"]').type('999');
-            cy.get('[data-test="inputComplemento"]').type('Irmãos salvatori')
-            cy.get('[data-test="inputEstado"]').type('BA')
-            cy.contains('button','Cadastrar').click()
-            cy.location('pathname').should('equal', '/login');
-      
-            
+            cy.get("@clinicaJson").then(clinica =>{
+                cy.get('[href="/cadastro"]').click();
+
+                cy.get('[data-test="inputNome"]').type(clinica[0].nome);//falar sobre uso em array
+                cy.get('[data-test="inputCNPJ"]').type(clinica[0].cnpj);
+                cy.get('[data-test="inputEmail"]').type(clinica[0].email);
+                cy.get('[data-test="inputSenha"]').type(clinica[0].senha, {log: false}); //não exibir senha no log
+                cy.get('[data-test="inputSenhaVerificada"]').type(clinica[0].senha);
+                cy.get('.sc-bcXHqe').click()
+
+
+                cy.get('[data-test="inputTelefone"]').type('9999999999');
+                cy.get('[data-test="inputCEP"]').type('99999999');
+                cy.get('[data-test="inputRua"]').type('Salvatori')
+                cy.get('[data-test="inputNumero"]').type('999');
+                cy.get('[data-test="inputComplemento"]').type('Irmãos salvatori')
+                cy.get('[data-test="inputEstado"]').type('BA')
+                cy.contains('button','Cadastrar').click()
+                cy.location('pathname').should('equal', '/login');
+
+            })
+           
+                 
           })
 
        })
-    
     
     })
--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -25,4 +25,28 @@ beforeEach(() =>{
       cy.get(':nth-child(3) > .MuiStepLabel-root > .MuiStepLabel-iconContainer > .sc-hiDMwi').should('be.visible') //teste de componente, não será abordado no curso -> para saber mais
     })
 
+
+    it('cadastra usuário', () =>{
+      cy.get('[href="/cadastro"]').click();
+      cy.get('[data-test="inputNome"]').type('Catarina P');
+      cy.get('[data-test="inputCNPJ"]').type('12598432');
+      cy.get('[data-test="inputEmail"]').type('catarina@email.com');
+      cy.get('[data-test="inputSenha"]').type('Senha123');
+      cy.get('[data-test="inputSenhaVerificada"]').type('Senha123');
+      cy.get('.sc-bcXHqe').click()
+      cy.get('[data-test="inputTelefone"]').type('9999999999');
+      cy.get('[data-test="inputCEP"]').type('99999999');
+      cy.get('[data-test="inputRua"]').type('Salvatori')
+      cy.get('[data-test="inputNumero"]').type('999');
+      cy.get('[data-test="inputComplemento"]').type('Irmãos salvatori')
+      cy.get('[data-test="inputEstado"]').type('BA')
+      cy.contains('Cadastrar').click()
+      cy.location('pathname').should('equal', '/login');
+
+      
+    })
+    //http://localhost:3000/login
+    //(fetch)POST 200 http://localhost:8080/clinica
+    //consumir fixtures para inserir os dados
+    //falso positivo -> api não deveria deixar cadastrar dois usuários iguais (como comunicar isso ao time? Criando um teste com usuário já cadastrado e demonstrando a falha na api.)
 })
