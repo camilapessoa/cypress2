4dc48fe403cf7de743cd083345cf5a224a2df061
camilapessoa <camilabentespessoa@gmail.com>
2024-03-27 18:41:25 -0300

    ci com github actions e cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    
     // defaultCommandTimeout: 60000,
         setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -19,6 +20,7 @@ module.exports = defineConfig({
         json: false,
         timestamp: "mmddyyyy_HHMMss" 
     },
+    projectId: "yfnfzp"
     
   }
 });
